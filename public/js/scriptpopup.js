43d40bcc3f1e100266bc465dac6b72fd6d5873d1
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2022-11-30 14:04:54 +0100

    popup fetch

--- public/js/scriptpopup.js
+++ public/js/scriptpopup.js
@@ -2,22 +2,20 @@ document.querySelectorAll('.res-button')
 
 
 const crossup = document.querySelectorAll(".pop-cross") //on récupere toutes les croix de cloture
-const popcont = document.querySelectorAll(".popup_container")//on récupère toutes les pop ups
 
 //on récupère tout les bouton de réservations
-const mesboutons = document.querySelectorAll('.pop-button')
+const mesboutons = document.querySelectorAll('.resButton')
+
 //on récupère toutes les popups
-const mespopup = document.querySelectorAll('.popup_container')
+const mespopup = document.querySelectorAll('.popupbox')
 
 mesboutons.forEach(function(bouton){
     bouton.addEventListener('click', function(){
-        mespopup.forEach(function(popup){
-            if(bouton.id == popup.id){
-                popup.style.display = 'block'
-            }
+        console.log('un clique')
+        mespopup.style.display ="block";
+
         })
     })
-})
 
 //Si action sur une croix, on déclenche la fonction Hidepop
 crossup.forEach(function (button) {
@@ -27,7 +25,7 @@ crossup.forEach(function (button) {
 
 // vvv
 function hidepop(){
-    popcont.forEach(function(item){
+    mespopup.forEach(function(item){
         item.style.display="none";
     })
 }
