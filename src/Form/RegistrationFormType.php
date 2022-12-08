458bacdb2ef3fd00f3c572a5f0bfc8a0cd1442ac
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2022-12-08 15:43:09 +0100

    registration form input and css

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,7 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Clients;
+use phpDocumentor\Reflection\Type;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,7 +19,8 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('client_email')
+            ->add('client_prenom', TextType::class, ['attr' => ['class' => 'register_input']])
+            ->add('client_email', TextType::class, ['attr' => ['class' => 'register_input']])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -30,7 +33,7 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password', 'class' => 'register_input'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
